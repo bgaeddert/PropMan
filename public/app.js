7a4732154cc2ce4ed876e2f0f2222afb4032bd66
Brian Gaeddert <bgaeddert@gmail.com>
2015-08-16 21:50:19 -0600

    Added where function

--- public/app.js
+++ public/app.js
@@ -115,3 +115,38 @@
     });
 
 })();
+
+
+/**
+ * Returns a object in an array of objects
+ * that has a property whose value matches
+ * a string (find)
+ *
+ * @param objectArray
+ * @param property
+ * @param find
+ * @returns {*}
+ */
+function where(objectArray, property, find){
+
+    if( ! objectArray )      throw 'Argument 1 is required.';
+    if( ! property )    throw 'Argument 2 is required.';
+    if( ! find )        throw 'Argument 3 is required.';
+    if( typeof objectArray !== 'object' ) throw 'Argument 1 requires an objectArray but a ' + typeof object + ' provided.';
+    if( typeof property !== 'string' ) throw 'Argument 2 requires a string but a ' + typeof property + ' provided.';
+    if( typeof find !== 'string' ) throw 'Argument 3 requires a string but a ' + typeof find + ' provided.';
+
+    var index = null;
+
+    objectArray.map(function (value, key) {
+        if(value.hasOwnProperty(property)){
+            if(value.id == find){
+                index = key;
+            }
+        }else{
+            return -1;
+        }
+    });
+
+    return objectArray[index];
+}
